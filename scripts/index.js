b68dd32198540e2a858747cd100eaacc6693b07d
Isabella Jorissen <ifjorissen@gmail.com>
2015-04-13 18:31:57 -0700

    trying to implement some prototypes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 //Initialize shell
-var shell = require("gl-now")({tickRate:1000})
+var shell = require("gl-now")({tickRate:1000, frameTime:0.0})
 var fs = require("fs")
 
 var VERT_SRC = fs.readFileSync(__dirname +  '/shaders/vert_shader.glsl', 'utf8')
@@ -7,7 +7,121 @@ var FRAG_SRC = fs.readFileSync(__dirname + '/shaders/frag_shader.glsl', 'utf8')
 
 var shader
 var vertexPositions
+var vertexVelocity
+var vertexAcceleration
 var vertexColors
+
+function updatePositions(){
+  var movement = [0., -.01, 0.]
+  for(var i=0; i<3; ++i) {
+    vertexPositions[i*2 + 1] += movement[1]
+  }
+}
+
+System = function (num){
+  this.init(num)
+}
+
+System.prototype = {
+  init: function(num){
+    var gl = shell.gl
+    this.color_attribute = gl.getAttribLocation(shader, "a_color")
+    this.position_attribute = gl.getAttribLocation(shader, "a_position")
+    // this.velocity_uniform = gl.getUniformLocation(shader, "u_velocity")
+    this.numBodies = num || 2
+    this.colors = [
+      [1,1,1],
+      [0.3451, 1.0, 0.5450],
+      [1.0, 0.4313, 0.3411],
+      [1.0, 0.8862, 0.3725],
+      [0.3804, 0.7647, 1.0]
+    ]
+    this.bodies = []
+    for(var i=0; i<=this.numBodies; ++i){
+      var c = this.colors[(Math.random()*this.colors.length)|0]
+      var p = [.8-Math.random()*1.6, .8-Math.random()*1.6]
+      this.bodies.push(new Body(p[0], p[1],c, 75))
+    }
+    console.log(this.bodies)
+    this.PitemSize = 2
+    this.CitemSize = 3
+
+    this.vertexPositions = new Float32Array(this.numBodies*this.PitemSize)
+    this.vertexColors    = new Float32Array(this.numBodies*this.CitemSize)
+    for(var i=0; i<=this.numBodies; ++i){
+      this.vertexPositions.push(this.bodies[i].x)
+      this.vertexPositions.push(this.bodies[i].y)
+      for (var j; j<this.CitemSize; ++j){
+        this.vertexColors.push(this.bodies[i].c[j])
+      }
+    }
+
+  },
+  tick: function(){
+    updatePositions()
+  },
+  updateBuffers: function(){
+    console.log("updateBuffers called")
+    var gl = shell.gl
+
+    this.vertexPositions = new Float32Array(this.numBodies*this.PitemSize)
+    for(var i=0; i<=this.numBodies; ++i){
+      this.vertexPositions.push(this.bodies[i].x)
+      this.vertexPositions.push(this.bodies[i].y)
+    }
+    var colorBuf = gl.createBuffer()
+    var vertBuf = gl.createBuffer()
+
+    // var color_attribute = gl.getAttribLocation(shader, "a_color")
+    // var position_attribute = gl.getAttribLocation(shader, "a_position")
+    // var velocity_uniform = gl.getUniformLocation(shader, "u_velocity")
+    // shell.movementLoc = velocity_uniform
+
+    gl.enableVertexAttribArray(color_attribute)
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuf)
+    gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW)
+    gl.vertexAttribPointer(this.color_attribute, 3, gl.FLOAT, false, 0, 0)
+
+    gl.enableVertexAttribArray(position_attribute)
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf)
+    gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STREAM_DRAW)
+    gl.vertexAttribPointer(this.position_attribute, 2, gl.FLOAT, false, 0, 0)
+    gl.drawArrays(gl.POINTS, 0, 3)
+    gl.disableVertexAttribArray(vertBuf)
+    gl.disableVertexAttribArray(colorBuf)
+  }
+}
+
+Body = function(x, y, c, mass){
+  this.init(x,y,c,mass)
+}
+
+Body.prototype = {
+  init: function(x,y,c, mass){
+    this.x = x || 0.0
+    this.y = y || 0.0
+    this.c = c
+    // this.vx = vx || 0.0
+    // this.vy = vy || 0.0
+    // this.ax = ax || 0.0
+    // this.ay = ax || 0.0
+    this.mass = mass || 50
+  }
+  // tick: function(){
+  //   this.x += this.vx
+  //   this.y += this.vy
+  //   this.vx = 
+  //   this.vy = 
+  //   this.ax = 
+  //   this.ay = 
+  // }
+}
+
+// function bindAttribBuf(data, buffer){
+// }
+// function gravity(){
+
+// }
 
 shell.on("gl-init", function() {
   console.log("gl-init called")
@@ -31,6 +145,9 @@ shell.on("gl-init", function() {
   gl.linkProgram(shader)
   gl.useProgram(shader)
 
+  // shell.sim = new System(3)
+  // shell.sim.init()
+  // shell.sim.updateBuffers()
   var colorArray = [
     [1,1,1],
     [0.3451, 1.0, 0.5450],
@@ -40,21 +157,20 @@ shell.on("gl-init", function() {
   ]
 
   var numPoints = 3
-  var PitemSize = 3
+  var PitemSize = 2
   var CitemSize = 3
 
   vertexPositions = new Float32Array(numPoints*PitemSize)
   vertexColors    = new Float32Array(numPoints*CitemSize)
 
-  //   var vertexPBuffer = createBuffer(gl, vertexPositions)
-  //   var vertexCBuffer = createBuffer(gl, vertexColors)
+    // var vertexPBuffer = createBuffer(gl, vertexPositions)
+    // var vertexCBuffer = createBuffer(gl, vertexColors)
 
-  // refine this or scrap it, assigns random colors and positions to the bodies
   var cPointer = 0
   var pPointer = 0
   for(var i=0; i<=numPoints; ++i) {
     var c = colorArray[(Math.random()*colorArray.length)|0]
-    var p = [.8-Math.random()*1.6, .8-Math.random()*1.6, .8-Math.random()*1.6]
+    var p = [.8-Math.random()*1.6, .8-Math.random()*1.6]
     for(var j=0; j<=CitemSize; ++j) {
       vertexColors[cPointer] = c[j]
       cPointer += 1
@@ -64,21 +180,6 @@ shell.on("gl-init", function() {
       pPointer += 1
     }
   }
-
-//   var vertexArray = createVAO(gl, [
-//     {
-//       "buffer": vertexPBuffer,
-//       "size": PitemSize
-//     },
-//     {
-//       "buffer": vertexCBuffer,
-//       "size": CitemSize
-//     }
-//   ])
-
-  // Create buffer
-  // var colors = [.1,0.,1, 1.,0,1., 0.,1.,1.]
-  // var positions = [.5, .5, 0., .75, 0, 0., 0., 0., 0.]
 
   var colorBuf = gl.createBuffer()
   var vertBuf = gl.createBuffer()
@@ -96,75 +197,38 @@ shell.on("gl-init", function() {
   gl.enableVertexAttribArray(position_attribute)
   gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf)
   gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STREAM_DRAW)
-  gl.vertexAttribPointer(position_attribute, 3, gl.FLOAT, false, 0, 0)
-
+  gl.vertexAttribPointer(position_attribute, 2, gl.FLOAT, false, 0, 0)
 })
 
-shell.on("tick", function(){
-  console.log("tick")
-  var gl = shell.gl
-  movement = [0., -.1, 0.]
-  // movement[1] += -.1
-  gl.uniform3fv(gl.getUniformLocation(shader, "u_velocity"), movement)
-
-  // var position_attribute = gl.getAttribLocation(shader, "a_position")
-  // var color_attribute = gl.getAttribLocation(shader, "a_color")
-  // var vertBuf = gl.createBuffer()
-  // var colorBuf = gl.createBuffer()
-  // for(var i=0; i<=3; ++i) {
-  //   vertexPositions[i*3 + 1] += movement
-  // }
-  // gl.enableVertexAttribArray(position_attribute)
-  // gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf)
-  // gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STREAM_DRAW)
-  // gl.vertexAttribPointer(position_attribute, 3, gl.FLOAT, false, 0, 0)
-
-  // gl.enableVertexAttribArray(color_attribute)
-  // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuf)
-  // gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW)
-  // gl.vertexAttribPointer(color_attribute, 3, gl.FLOAT, false, 0, 0)
-  // gl.drawArrays(gl.POINTS, 0, 3)
-})
-
-// var tick = function(){
-//   console.log("???tick")
-
-//   var gl = shell.gl
-//   movement = [0., -.1, 0.]
-//   movement[1] += -.1
-//   gl.uniform3fv(gl.getUniformLocation(shader, "u_velocity"), movement)
-
-//   var position_attribute = gl.getAttribLocation(shader, "a_position")
-
-//   gl.enableVertexAttribArray(position_attribute)
-//   gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf)
-//   // gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STREAM_DRAW)
-//   gl.vertexAttribPointer(position_attribute, 3, gl.FLOAT, false, 0, 0)
-// }
 
 shell.on("gl-render", function(t) {
   console.log("render called")
   var gl = shell.gl
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
-  movement = [0., -.01, 0.]
-  // movement[1] += -.1
-  gl.uniform3fv(gl.getUniformLocation(shader, "u_velocity"), movement)
   var position_attribute = gl.getAttribLocation(shader, "a_position")
   var color_attribute = gl.getAttribLocation(shader, "a_color")
   var vertBuf = gl.createBuffer()
   var colorBuf = gl.createBuffer()
-  for(var i=0; i<3; ++i) {
-    vertexPositions[i*3 + 1] += movement[1]
-  }
+
+  // gl.uniform3fv(gl.getUniformLocation(shader, "u_velocity"), movement)
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  // sim.updateBuffers()
+  updatePositions()
+  console.log(shell)
+  shell.paused = true
+  
+  movement = [0., -.01, 0.]
+
   gl.enableVertexAttribArray(position_attribute)
   gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf)
   gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STREAM_DRAW)
-  gl.vertexAttribPointer(position_attribute, 3, gl.FLOAT, false, 0, 0)
+  gl.vertexAttribPointer(position_attribute, 2, gl.FLOAT, false, 0, 0)
 
   gl.enableVertexAttribArray(color_attribute)
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuf)
   gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW)
   gl.vertexAttribPointer(color_attribute, 3, gl.FLOAT, false, 0, 0)
+
   //Draw the points
   gl.drawArrays(gl.POINTS, 0, 3)
   gl.disableVertexAttribArray(vertBuf)
--- scripts/shaders/vert_shader.glsl
+++ scripts/shaders/vert_shader.glsl
@@ -1,7 +1,7 @@
 precision highp float;
 precision highp int;
 
-attribute vec3 a_position;
+attribute vec2 a_position;
 attribute vec3 a_color;
 
 uniform vec3 u_velocity;
@@ -9,7 +9,7 @@ varying vec3 fragColor;
 
 void main() {
   // gl_Position = vec4(a_position+u_velocity), 1.0);
-  gl_Position = vec4(a_position, 1.0);
+  gl_Position = vec4(a_position, 1.0, 1.0);
   gl_PointSize = 75.0;
   fragColor = a_color;
 }
