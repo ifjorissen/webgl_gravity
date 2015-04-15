e7bf6a994d6d61937eaa8b18f6f3ac29274fb675
Isabella Jorissen <ifjorissen@gmail.com>
2015-04-14 18:30:46 -0700

    this works for n bodies

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
 //Initialize shell
 var shell = require("gl-now")({tickRate:1000, frameTime:.99, frameSkip:10000})
 var fs = require("fs")
+var add = require("vectors/add-nd")
+var mag = require("vectors/mag-nd")
+var sub = require("vectors/sub-nd")
+var dist = require("vectors/dist-nd")
+var div = require("vectors/div-nd")
+var dot = require("vectors/dot-nd")
+var mult = require("vectors/mult-nd")
+var norm = require("vectors/normalize-nd")
+var copy = require("vectors/copy-nd")
 
 var VERT_SRC = fs.readFileSync(__dirname +  '/shaders/vert_shader.glsl', 'utf8')
 var FRAG_SRC = fs.readFileSync(__dirname + '/shaders/frag_shader.glsl', 'utf8')
@@ -24,7 +33,7 @@ System.prototype = {
 
     this.numBodies = parseInt(num) || 4
     this.colors = [
-      [0,0,0],
+      [0.0,0.0,0.0],
       [0.3451, 1.0, 0.5450],
       [1.0, 0.4313, 0.3411],
       [1.0, 0.8862, 0.3725],
@@ -35,52 +44,63 @@ System.prototype = {
     ]
 
     this.bodies = []
-    this.bodies.push(new Body([0.0, 0.0], [0.0,0.0], this.colors[0], 1000))
+    this.bodies.push(new Body([0.0, 0.0], [0.0,0.0], this.colors[0], 1000, 0))
     for(var i=0; i<this.numBodies-1; ++i){
       var c = this.colors[(Math.random()*this.colors.length)|0]
       var p = [.8-Math.random()*1.6, .8-Math.random()*1.6]
       var v = [.1-Math.random()*.2, .1-Math.random()*.2]
       var m = Math.random()*5
       this.bodies.push(new Body(p, v, c, m, i))
+      console.log("pinit" + this.bodies[i].p)
     }
+     console.log("pinit" + this.bodies[3].p)
     this.PitemSize = 2
     this.CitemSize = 3
 
-    var vp = new Float32Array(this.numBodies*this.PitemSize)
-    var vc = new Float32Array(this.numBodies*this.CitemSize)
+    // var vp = new Float32Array(this.numBodies*this.PitemSize)
+    // var vc = new Float32Array(this.numBodies*this.CitemSize)
+    var vp = []
+    var vc = []
 
     var cPointer = 0;
     var pPointer = 0;
     for(var i=0; i<this.numBodies; ++i){
+      var pos = this.bodies[i].p
       for (var k=0; k<this.CitemSize; ++k){
-        vc[cPointer] = this.bodies[i].c[k]
-        cPointer += 1
+        // vc[cPointer] = this.bodies[i].c[k]
+        // cPointer += 1
+        vc.push(this.bodies[i].c[k])
       }
       for(var j=0; j<this.PitemSize; ++j){
-        vp[pPointer] = this.bodies[i].p[j]
-        pPointer += 1
+        // vp[pPointer] = pos[j]
+        // pPointer += 1
+        vp.push(this.bodies[i].p[j])
       }
     }
     this.vertexPositions = vp
     this.vertexColors    = vc
-    this.gvec = new Array(this.numBodies*this.PitemSize)
+    this.gvec = new Array(this.numBodies)
 
   },
   gravity: function(){
     console.log("gravity!")
-    this.gvec = new Array(this.numBodies*this.PitemSize).fill(0)
+    this.gvec = new Array(this.numBodies)
     for(var i=0; i<this.numBodies; ++i){
       for(var j=0; j<this.numBodies; ++j){
-        console.log(this.gvec)
+        this.gvec[i] = [0,0]
         if (j!= i){
-          var dist = [(this.bodies[j].p[0] - this.bodies[i].p[0]), (this.bodies[j].p[1] - this.bodies[i].p[1])]
-          var dir = Math.sqrt(dist[0]*dist[0] + dist[1]*dist[1])
-          this.gvec[i*this.PitemSize + 0] += this.bodies[j].mass/dist[0]*dir
-          this.gvec[i*this.PitemSize + 1] += this.bodies[j].mass/dist[1]*dir
-          console.log(this.gvec[i*this.PitemSize + 1])
-          console.log(i*this.PitemSize + 0)
-          console.log(i*this.PitemSize + 1)
-          console.log(this.gvec)
+          var tbip = copy(this.bodies[i].p)
+          var tbjp = copy(this.bodies[j].p)
+          var dist = copy(tbjp)
+          sub(dist, tbip)
+          var dlen = mag(dist)
+          var dir = copy(dist)
+          norm(dir)
+          var agj = copy(dir)
+          mult(agj, this.bodies[j].mass/dlen*dlen)
+          add(this.gvec[i], agj)
+          console.log("p for j" + this.bodies[j].p)
+          console.log("p for i" + this.bodies[i].p)
         }
       }
     }
@@ -90,13 +110,16 @@ System.prototype = {
     console.log("updateBuffers called")
     var gl = shell.gl
     this.gravity()
-    vp = new Float32Array(this.numBodies*this.PitemSize)
+    // vp = new Float32Array(this.numBodies*this.PitemSize)
+    var vp = []
     var pPointer = 0;
     for(var i=0; i<this.numBodies; ++i){
-      this.bodies[i].update(this.gvec[i*this.PitemSize + 0], this.gvec[i*this.PitemSize + 1])
+      this.bodies[i].update(this.gvec[i])
+      var pos = this.bodies[i].p
       for(var j=0; j<this.PitemSize; ++j){
-        vp[pPointer] = this.bodies[i].p[j]
-        pPointer += 1
+        // vp[pPointer] = pos[j]
+        // pPointer += 1
+        vp.push(this.bodies[i].p[j])
       }
     }
     this.vertexPositions = vp
@@ -114,13 +137,13 @@ Body.prototype = {
     this.c = c
     this.mass = mass || 50
     this.id = i
-    this.h = 1000
+    this.h = 100
   },
-  update: function(gvec0, gvec1){
-    this.p[0] += this.v[0]/this.h
-    this.p[1] += this.v[1]/this.h
-    this.v[0] += gvec0/this.h
-    this.v[1] += gvec1/this.h
+  update: function(gvec){
+    var vtmp = copy(this.v)
+    var accg = copy(gvec)
+    add(this.p, div(vtmp, this.h))
+    add(this.v, div(accg, this.h))
   }
 }
 
@@ -146,8 +169,7 @@ shell.on("gl-init", function() {
   gl.linkProgram(shader)
   gl.useProgram(shader)
 
-  sim = new System(10)
-  sim.init()
+  sim = new System(6)
   sim.updateBuffers()
 })
 
@@ -162,12 +184,12 @@ shell.on("gl-render", function(t) {
 
   gl.enableVertexAttribArray(sim.position_attribute)
   gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf)
-  gl.bufferData(gl.ARRAY_BUFFER, sim.vertexPositions, gl.STREAM_DRAW)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sim.vertexPositions), gl.STREAM_DRAW)
   gl.vertexAttribPointer(sim.position_attribute, sim.PitemSize, gl.FLOAT, false, 0, 0)
 
   gl.enableVertexAttribArray(sim.color_attribute)
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuf)
-  gl.bufferData(gl.ARRAY_BUFFER, sim.vertexColors, gl.STATIC_DRAW)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sim.vertexColors), gl.STATIC_DRAW)
   gl.vertexAttribPointer(sim.color_attribute, sim.CitemSize, gl.FLOAT, false, 0, 0)
 
   //Draw the points
